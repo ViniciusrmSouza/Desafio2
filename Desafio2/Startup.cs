4f1b15727a40e3dbe21878805a9a2cd722c37919
Vinicius Souza <viniciusrmsouza577@gmail.com>
2021-11-21 12:46:59 -0300

    Adicionado o swagger

--- Desafio2/Startup.cs
+++ Desafio2/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +27,22 @@ namespace Desafio2
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Desafio",
+                    Description = "Desafio do estagio",
+                    TermsOfService = new Uri("http://dontpad.com/termsserv"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Vinicius Souza",
+                        Email = "viniciusrmsouza577@gmail.com",
+                        Url = new Uri("https://github.com/ViniciusrmSouza")
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +52,14 @@ namespace Desafio2
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Desafio");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
 
