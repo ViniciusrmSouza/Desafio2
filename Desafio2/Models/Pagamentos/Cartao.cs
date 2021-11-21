df83a0e870bf8d176bcd929c0ffb06b2ce7b32b4
Vinicius Souza <viniciusrmsouza577@gmail.com>
2021-11-21 12:36:00 -0300

    Criado classes Compras e Cartao

---
+++ Desafio2/Models/Pagamentos/Cartao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Desafio2.Models.Pagamentos
+{
+    public class Cartao
+    {
+        public string Titular { get; set; }
+        public int Numero { get; set; }
+        public string Data_Expiracao { get; set; }
+        public string Bandeira { get; set; }
+        public int Cvv { get; set; }
+    }
+}
---
+++ Desafio2/Models/Pagamentos/Compras.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Desafio2.Models.Pagamentos
+{
+    public class Compras
+    {
+        public Guid  Id { get; set; }
+        public double Valor { get; set; }
+        public Cartao Cartao { get; set; }
+    }
+}
